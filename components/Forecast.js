6f402074d6dc2dd30e74837894e8a5ae72f28b68
5910110423 <5910110423@psu.ac.th>
2018-09-24 14:06:04 +0700

    จัด UI

--- components/Forecast.js
+++ components/Forecast.js
@@ -5,20 +5,45 @@ export default class Forecast extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>{this.props.main}</Text>
-        <Text>{this.props.description}</Text>
-        <Text>{this.props.temp}</Text>
-        <Text>°C</Text>
+        <Text style={styles.textMain}>{this.props.main}</Text>
+        <Text style={styles.textDescription}>{this.props.description}</Text>
+        <View style={styles.flexTemp}>
+          <Text style={styles.textTemp}>{this.props.temp}</Text>
+          <Text style={styles.TextSymboltemp}> °C</Text>
+        </View>
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
-    container: {
-        paddingTop:25
-    },
-    backdrop : {
-        width: '100%',
-        height: '100%'
+  container: {
+    flex: 1
+  },
+  textMain: {
+    fontSize: 40,
+    marginTop: 10,
+    flex: 1,
+    color: "white",
+    textAlign: "center"
+  },
+  textDescription: {
+    fontSize: 20,
+    flex: 1,
+    color: "white",
+    textAlign: "center"
+  },
+  textTemp: {
+    fontSize: 30,
+    color: "white"
+  },
+  flexTemp: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  TextSymboltemp: {
+    fontSize: 20,
+    color: "white",
+    marginTop: 5
   }
-  });
+});
--- components/Weather.js
+++ components/Weather.js
@@ -11,26 +11,68 @@ export default class Weather extends React.Component {
                 description:'-',
                 temp : 0
             }
-        }
+        };
     }
-  render() {
+
+    fetchData = () =>{
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.props.zipCode},th&units=metric&APPID=fd68c0f2039c5a25f666a9ff374bc93e`)
+        .then((response)=> response.json())
+        .then((json)=>{
+            this.setState(
+                {
+                    forecast : {
+                        main : json.weather[0].main,
+                        fescription : json.Weather[0].description,
+                        temp : json.main.temp
+                    }
+                });
+        })
+        .catch((error) => {
+            console.warn(error);
+        });
+    }
+    
+    componentDidMout = ()=> this.fetchData() 
+    componentDidUpdate = (prevProps) => {
+        if(prevProps.zipCode !== this.props.zipCode){
+          this.fetchData()
+        }
+      }
+
+render() {
     return (
       <View style={styles.container}>
       <ImageBackground source = {require('../bg1.jpg')} style={styles.backdrop}>
-        <Text>Zip code is {this.props.zipCode}.</Text>
-        <Forecast {...this.state.forecast}/>
+        <View style={styles.top}>
+            <Text style={styles.text} >Zip code is {this.props.zipCode}.</Text>
+            <Forecast {...this.state.forecast}/>
+        </View>
       </ImageBackground>
       </View>
-    );
+    );  
   }
 }
 
+
+
 const styles = StyleSheet.create({
-  container: {
-      paddingTop:25
-  },
-  backdrop : {
+    container: {
+      paddingTop:10
+    },
+    backdrop : {
       width: '100%',
       height: '100%'
-}
+    },
+    top: {
+        backgroundColor: "black",
+        flexDirection: "column",
+        opacity: 0.5,
+        height: "50%"
+    },
+    text: {
+        fontSize: 20,
+        marginTop: 20,
+        color: "white",
+        textAlign: "center",
+      }
 });
